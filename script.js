3140b6a31d62d987f5904fb310068d5921a75e11
Amoorer75 <Amoorer127@gmail.com>
2021-10-31 04:20:00 -0400

    some css and js  chages

--- script.js
+++ script.js
@@ -2,31 +2,45 @@ const flashcards = document.getElementsByClassName("Flashcards")[0];
 const createBox = document.getElementsByClassName("create-box")[0];
 const question = document.getElementById('question');
 const answer = document.getElementById('answer');
-let contentArray = []
+let contentArray = [];
 
-contentArray.forEach(divMaker);
+//construct the flashcard
+//tells the flashcard what should be on it
 function divMaker(text){
     let div = document.createElement("Div");
     let h2_question = document.createElement("H2");
     let h2_answer = document.createElement("H2");
+    let clickMe = document.createElement("H2")
+
+   
 
     div.className = 'flashcard';
 
     h2_question.setAttribute('style',"border-top: 1px solid red; padding: 15px; margin-top: 30px");
-
+    //this is giving the question that was stored a tag of h2  
     h2_question.innerHTML = text.myQuestion
 
-    h2_answer.setAttribute('style', "text-align:center; display:none; color: green")
-
+    h2_answer.setAttribute('style', "text-align:center; display:none; color: red")
+    //this is giving the answer that was stored a tag of h2 
     h2_answer.innerHTML = text.myAnswer
 
+    clickMe.setAttribute('style', "text-align:center; color: red")
+    //
+    clickMe.innerHTML = "Click to show answer"
+
+    //setting up the order it should apear in the flashcard 
     div.appendChild(h2_question);
     div.appendChild(h2_answer);
+    div.appendChild(clickMe)
 
+    //hides the answer untill the card is click 
+    //then the answer will show 
     div.addEventListener("click", function(){
         if(h2_answer.style.display == "none"){
+            clickMe.style.display = "none"
             h2_answer.style.display = "block";
         }else{
+            clickMe.style.display = "block"
             h2_answer.style.display = "none";
         }
     })
@@ -34,30 +48,36 @@ function divMaker(text){
     flashcards.appendChild(div);
 }
 
-
+//make the flashcard when click on save 
 function addFlashcard(){
+    // storing the info of questions and answers in an object
    let flashcard_info = {
         'myQuestion' : question.value,
         'myAnswer' : answer.value
     }
-
+    //place the object into an array 
     contentArray.push(flashcard_info);
 
+    //display what you just enter by picking the
+    //last object that was push to the array
     divMaker(contentArray[contentArray.length - 1]);
+    //reset the text fields 
     question.value = ''
     answer.value = ''
 }
 
+//removes all the current flash cards you have up 
 function delFlashcardBox(){
-    localStorage.clear();
-    flashcards.innerHTML =  '';
+    flashcards.innerHTML = '';
     contentArray = [];
 }
 
+//when you click on new card the create falashcard box appears
 function showCreateCardBox(){
     createBox.style.display = "block";
 }
 
+//when you click close the create flashcard box hides
 function hideCreateBox(){
     createBox.style.display = "none";
 }
